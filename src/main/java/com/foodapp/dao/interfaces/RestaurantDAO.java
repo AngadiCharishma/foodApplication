20f1299a861651b94f57d70cc93fa570afdb46a8
AngadiCharishma <136798407+AngadiCharishma@users.noreply.github.com>
2025-02-28 22:24:35 +0530

    Update RestaurantDAO.java

--- src/main/java/com/foodapp/dao/interfaces/RestaurantDAO.java
+++ src/main/java/com/foodapp/dao/interfaces/RestaurantDAO.java
@@ -1,1 +1,26 @@
 
+package com.foodapp.dao.interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.foodapp.model.Restaurant;
+
+public interface RestaurantDAO {
+	
+           int insert(Restaurant r); 
+		   List<Restaurant> fetchAll();
+		  
+		   Restaurant fetchOne(int i);  
+		   int update(int id,float ratings);
+		   int delete(int id);
+		   
+		}
+
+
+
+
+
+
+
+
